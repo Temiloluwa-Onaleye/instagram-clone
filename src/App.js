d44867b3e06cb6025daffab818e19ef9632f6ca9
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2021-07-15 12:14:33 +0100

    instagram clone

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Post from './Post';
 import { db } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import { Button } from '@material-ui/core';
+import { Button} from '@material-ui/core';
+import { Input } from '@material-ui/core';
 
 function getModalStyle() {
   const top = 50;
@@ -33,6 +34,9 @@ function App() {
   const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState([]);
 const [open, setOpen] = useState(false);
+const [username, setUsername] = useState(' ');
+const [email, setEmail] = useState(' ');
+const [password, setPassword] = useState(' ');
   useEffect(() => {
    db.collection('posts').onSnapshot(snapshot => {
      setPosts(snapshot.docs.map(doc => ({id: doc.id, post: doc.data()})));
@@ -48,7 +52,34 @@ const [open, setOpen] = useState(false);
     onClose={() => setOpen(false)}
   >
   <div style={modalStyle} className={classes.paper}>
-  <h2>I am a Modal</h2>
+  <center>
+  <img 
+    className="app_headerImage"
+    src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+    alt=""
+    />
+    </center>
+    <form>
+    <input
+    placeholder = "username"
+    type="text"
+    value={username}
+    onChange={(e) => setUsername(e.target.value)}
+    />
+    <input
+    placeholder = "email"
+    type="text"
+    value={email}
+    onChange={(e) => setEmail(e.target.value)}
+    />
+    <input
+    placeholder = "password"
+    type="text"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+    />
+    <Button onClick={signUp}>Sign up</Button>
+    </form>
 </div>
   </Modal>
     {/*header */}

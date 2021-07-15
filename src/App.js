78a53955befcec5dbc40e159eeb9c22f1358a893
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2021-07-15 11:31:47 +0100

    instagram clone

--- src/App.js
+++ src/App.js
@@ -2,18 +2,55 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
 import { db } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState([]);
-
+const [open, setOpen] = useState(false);
   useEffect(() => {
    db.collection('posts').onSnapshot(snapshot => {
      setPosts(snapshot.docs.map(doc => ({id: doc.id, post: doc.data()})));
    })
   }, [ ]);
+  const signUp = (event) => {
+
+  }
   return (
     <div className="app">
-   
+    <Modal
+    open={open}
+    onClose={() => setOpen(false)}
+  >
+  <div style={modalStyle} className={classes.paper}>
+  <h2>I am a Modal</h2>
+</div>
+  </Modal>
     {/*header */}
     <div className="app_header">
     <img 
@@ -22,6 +59,8 @@ function App() {
     alt=""
     />
     </div>
+<Button onClick={() => setOpen(true)}>Sign up</Button>
+
     <h1>Temiloluwa Onaleye</h1>  
 
     {
